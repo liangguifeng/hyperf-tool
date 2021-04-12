c0193d7bd932fd7d8ba8f2925b21da36d2c6f6e1
liangguifeng <1476982312@qq.com>
2021-04-12 17:00:24 +0800

    异常处理

---
+++ src/Exception/ResourceException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of liangguifeng.
+ *
+ * @link     https://findcat.cn
+ * @document https://findcat.cn
+ * @contact  1476982312@qq.com
+ */
+namespace Tool\Exception;
+
+use Hyperf\Server\Exception\ServerException;
+
+class ResourceException extends ServerException
+{
+}
