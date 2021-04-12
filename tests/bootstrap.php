1f1cbbacd66c9c474e43118603e63e44ff523dc6
liangguifeng <1476982312@qq.com>
2021-04-12 12:05:24 +0800

    fix style

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of liangguifeng.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://findcat.cn
+ * @document https://findcat.cn
+ * @contact  1476982312@qq.com
  */
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
